1881211241f00a5fc771576c88afc46dc30e2117
Chachacharlie <TCCA_EDNS@hotmai.com>
2023-03-17 15:09:05 -0600

    implementacion de filtro, faltan retoques

--- src/components/TodasLasEntradas/TodasLasEntradas.jsx
+++ src/components/TodasLasEntradas/TodasLasEntradas.jsx
@@ -1,8 +1,9 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import '../../styles/blogEntrys.css'
 
-const Filtros = [ "Python 🐍", "Supercapacitores 🥽"]
+
+const Filtro = [ "Python 🐍", "Supercapacitores 🥽", "Materiales 🌡️"]
 
 const Entradas = [
     {
@@ -37,19 +38,35 @@ const Entradas = [
         tittle: "Entrada 5",
         date: "07/03/23",
         description: " lo",
-        tags: ["Supercapacitores 🥽", "Python 🐍"],
+        tags: [ "Python 🐍"],
         
     },
     {
         tittle: "Entrada 6",
         date: "05/03/23",
         description: " lo",
-        tags: ["Supercapacitores 🥽", "Python 🐍"],
+        tags: ["Supercapacitores 🥽"],
         
     },
 ]
 
+
+
 export const TodasLasEntradas = () =>{
+
+    const [ filtros, setFiltro ] = useState( [...Filtro] );
+
+    const handleFiltro = ( event ) => {
+        if(event.target.checked ){
+
+            setFiltro( (filtros) => [...filtros, event.target.value])
+        }else{
+            setFiltro( (filtros) => filtros.filter((elem) => elem !== event.target.value ))
+
+        }
+        
+    }
+    
     return(
         <section className="section-2">
             <div className="container">
@@ -57,28 +74,34 @@ export const TodasLasEntradas = () =>{
                 <br /><br />
                 <div className="row">
                     <div className="col-md-8">
+
                     {Entradas.map((elem,idx) =>{
-                        return(
+                    
+
+                        if( elem.tags.every( elem => filtros.includes(elem))) {
+                            return(
                             
-                            <div className="card w-75 mb-3 entry-log">
-                                <div className="card-body">
-                                    <div className="row">
-                                        <div className="col-md-8">
-                                            <h5 className="card-title fw-bolder color ">{elem.tittle}</h5>
-                                            <p className="card-text">Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
-                                            <p className="text-body-secondary">{elem.date}</p>
+                                <div className="card w-75 mb-3 entry-log">
+                                    <div className="card-body">
+                                        <div className="row">
+                                            <div className="col-md-8">
+                                                <h5 className="card-title fw-bolder color ">{elem.tittle}</h5>
+                                                <p className="card-text">Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+                                                <p className="text-body-secondary">{elem.date}</p>
+                                            </div>
+                                            <div className="col-md-4">
+                                            <ul> {elem.tags.map((tag) =>{return <li className="tag-entry text-body-secondary" key={elem.tittle}> {tag} </li> })}</ul>
+    
+                                            </div>
+            
                                         </div>
-                                        <div className="col-md-4">
-                                        <ul> {elem.tags.map((tag) =>{return <li className="tag-entry text-body-secondary" key={elem.tittle}> {tag} </li> })}</ul>
-
-                                        </div>
-        
+    
                                     </div>
-
                                 </div>
-                            </div>
+    
+                            )
+                        }
 
-                        )
                     })}
         
                     </div>
@@ -87,10 +110,10 @@ export const TodasLasEntradas = () =>{
                         <h4>Filtros</h4>
                         <div className="card " >
                             <div class="form-check filtros">
-                                {Filtros.map((filtro)=>{
+                                {Filtro.map((filtro)=>{
                                     return(
                                         <label>
-                                            <input type="checkbox" />
+                                            <input onChange={handleFiltro} value={filtro} type="checkbox" />
                                             <span>{filtro}</span>
                                         </label>
                                     )
